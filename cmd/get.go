42735248a863f4ab2d7ae1aa4c93486f955bbcce
HidemaruOwO <82384920+HidemaruOwO@users.noreply.github.com>
2022-07-20 16:35:52 +0900

    :sparkles: YouTube動画のダウンロード機能の実装

--- cmd/get.go
+++ cmd/get.go
@@ -1,1 +1,68 @@
 package cmd
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"path/filepath"
+
+	"github.com/fatih/color"
+	"github.com/kkdai/youtube/v2"
+	"github.com/spf13/cobra"
+
+	"github.com/hidemaruowo/ytac/lib"
+)
+
+var videoPath = ""
+
+func getCmd() *cobra.Command {
+	var cmd = &cobra.Command{
+		Use:   "get",
+		Short: "Download a video from YouTube",
+		Long:  `Download a video from YouTube`,
+		Run: func(cmd *cobra.Command, args []string) {
+			if args[0] != "" {
+				download(args[0])
+			} else {
+				var errorMessage string = color.HiRedString("🔥 Please provide a video ID")
+				fmt.Println(errorMessage)
+			}
+		},
+	}
+
+	return cmd
+}
+
+func download(videoID string) {
+	var client = youtube.Client{}
+
+	var thumbnail string = "https://img.youtube.com/vi/" + videoID + "/default.jpg"
+
+	video, err := client.GetVideo(videoID)
+	if err != nil {
+		panic(err)
+	}
+
+	var formats = video.Formats.WithAudioChannels() // only get videos with audio
+	stream, _, err := client.GetStream(video, &formats[0])
+	if err != nil {
+		panic(err)
+	}
+
+	videoPath = filepath.Join(lib.GetYtacPath(), "temp", video.Title+"-"+videoID+".mp4")
+
+	file, err := os.Create(videoPath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, stream)
+	if err != nil {
+		panic(err)
+	}
+	if lib.UseSixel() == true {
+		lib.ShowImage(thumbnail)
+	}
+}
